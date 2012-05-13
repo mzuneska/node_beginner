2cfe65b0512ce14ad257fe6a7f8d30e68a5cb757
Mark Zuneska <mark.zuneska@simonandschuster.com>
2012-05-13 17:32:19 -0400

    Set up a form and handle the data that is sent via a POST request.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,21 +1,29 @@
-var exec = require("child_process").exec;
+var queryString = require("querystring");
 
-function start(response) {
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
+  var docType = '<!DOCTYPE html>'
+  var body = '<html>'+
+               '<head>'+
+                 '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'+
+               '</head>'+
+               '<body>'+
+                 '<form action="/upload" method="post">'+
+                   '<textarea name="text" rows="20" cols="60"></textarea>'+
+                     '<input type="submit" value="Submit text" />'+
+                 '</form>'+
+               '</body>'+
+             '</html>';
 
-  exec("ls -lah", function(error, stdout, stderr) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write(stdout);
-    response.end();
-  });
-
-  return content;
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(docType + body);
+  response.end();
 }
 
-function upload() {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent the text: " + queryString.parse(postData).text);
   response.end(); 
 }
 
