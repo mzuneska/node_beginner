ab2de980d827d03db2605f705ed24d3690d58941
Mark Zuneska <mark.zuneska@simonandschuster.com>
2012-05-13 15:06:23 -0400

    Bring the response to everyone so as not to block further requests.
    Once a blocking action is done it can respond on its own.

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ var url = require("url");
 function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");  
-    
-    route(handle, pathname);
+    console.log("Request for " + pathname + " received.");    
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hi there");
-    response.end();
+    route(handle, pathname, response);
   }
   
   http.createServer(onRequest).listen(8888);
